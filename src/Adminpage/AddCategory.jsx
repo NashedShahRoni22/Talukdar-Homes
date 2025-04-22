f978333cf97ce518c61bc56852d608e6daf76851
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2025-04-23 00:52:49 +0600

    feat: category api tested

--- src/Adminpage/AddCategory.jsx
+++ src/Adminpage/AddCategory.jsx
@@ -1,25 +1,33 @@
-import { Button, Input, Option, Select } from "@material-tailwind/react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BiCheck, BiEdit, BiPlus } from "react-icons/bi";
-import { BsPlusCircle } from "react-icons/bs";
-import { CgAdd, CgClose } from "react-icons/cg";
+import { CgClose } from "react-icons/cg";
+import { ImSpinner } from "react-icons/im";
 import { MdDelete } from "react-icons/md";
 
 export default function AddCategory() {
   const [loader, setLoader] = useState(false);
   const [showEditBox, setShowEditBox] = useState(false);
-  const [category, setCategory] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [parentId, setParentId] = useState("");
   const [title, setTitle] = useState("");
-  // get category
-  useEffect(() => {
+  const [editingSubId, setEditingSubId] = useState("");
+  const [editedSubTitle, setEditedSubTitle] = useState("");
+
+  // Fetch all categories
+  const fetchCategories = () => {
+    setLoader(true);
     fetch("https://api.talukderhomes.com.au/api/categories")
       .then((res) => res.json())
-      .then((data) => setCategory(data.data));
-  }, []);
-  // add category
-  const handleAddCategory = async (e) => {
+      .then((data) => {
+        setLoader(false);
+        setCategories(data.data);
+      });
+  };
+
+  // Add new category
+  const handleAddCategory = async () => {
     setLoader(true);
+
     const data = {
       parent_id: parentId,
       title: title,
@@ -40,8 +48,11 @@ export default function AddCategory() {
       const result = await response.json();
       if (result.status === true) {
         window.alert(result.msg);
-        form.reset();
+        fetchCategories();
+        setShowEditBox(false);
         setLoader(false);
+        setTitle("");
+        setParentId("");
       }
     } catch (error) {
       console.log("Error:", error);
@@ -49,25 +60,73 @@ export default function AddCategory() {
     }
   };
 
-  // delete category 
-  const deleteCategory = async (categoryId) => {
+  // Update Sub-category
+  const updateSubCategory = async (parentId) => {
+    setLoader(true);
+
+    const payload = {
+      title: editedSubTitle,
+      parent_id: parentId,
+    };
+
     try {
-      const response = await fetch(`https://api.talukderhomes.com.au/api/categories/delete/${categoryId}`, {
-        method: 'GET', // Using GET (again, not recommended for deletion)
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to delete category');
+      const res = await fetch(
+        `https://api.talukderhomes.com.au/api/categories/update/${parentId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      const result = await res.json();
+
+      if (result.status === true) {
+        window.alert(result.msg);
+        fetchCategories();
+        setShowEditBox(false);
+        setLoader(false);
+        setTitle("");
+        setParentId("");
       }
-  
-      const data = await response.json();
-      window.alert(data.msg);
-    } catch (error) {
-      console.error('Error:', error);
+    } catch (err) {
+      console.log("Error:", err);
+      setLoader(false);
     }
   };
-  
-  
+
+  // Delete category
+  const deleteCategory = async (categoryId) => {
+    setLoader(true);
+
+    try {
+      const response = await fetch(
+        `https://api.talukderhomes.com.au/api/categories/delete/${categoryId}`,
+        {
+          method: "GET", // Using GET (again, not recommended for deletion)
+        }
+      );
+
+      if (!response.ok) {
+        setLoader(false);
+        throw new Error("Failed to delete category");
+      }
+
+      const data = await response.json();
+      window.alert(data.msg);
+      setLoader(false);
+      fetchCategories();
+    } catch (error) {
+      setLoader(false);
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   return (
     <div className="p-5">
@@ -89,10 +148,19 @@ export default function AddCategory() {
                 <CgClose className="text-orange-500 text-xl" />
               </button>
               <button
+                className={`p-1.5 rounded-full  ${
+                  loader
+                    ? "cursor-default bg-gray-100"
+                    : "cursor-pointer bg-red-50"
+                }`}
                 onClick={handleAddCategory}
-                className="p-1.5 rounded-full bg-red-100"
+                disabled={loader}
               >
-                <BiCheck className="text-red-500 text-xl" />
+                {loader ? (
+                  <ImSpinner className="text-lg text-gray-600 animate-spin" />
+                ) : (
+                  <BiCheck className="text-red-500 text-xl" />
+                )}
               </button>
             </div>
           </div>
@@ -107,35 +175,87 @@ export default function AddCategory() {
         )}
       </div>
       <div className="mt-5 grid grid-cols-2 lg:grid-cols-3 gap-5">
-        {category.map((c, i) => (
+        {categories.map((c, i) => (
           <div
             key={i}
             className="shadow rounded h-[350px] overflow-y-auto relative"
           >
             <div className="bg-primary text-white p-2.5 flex justify-between items-center">
               <h5 className=" text-center font-semibold">{c?.title}</h5>
-              <button onClick={()=> deleteCategory(c?.id)} className="p-2 shadow rounded-full bg-gray-50 text-red-500">
+              <button
+                className={`p-2 shadow rounded-full bg-gray-50 ${
+                  loader
+                    ? "text-gray-400 cursor-default"
+                    : "text-red-500 cursor-pointer"
+                }`}
+                onClick={() => deleteCategory(c?.id)}
+                disabled={loader}
+              >
                 <MdDelete className="text-xl" />
               </button>
             </div>
+
+            {/* Child Categories List */}
             <ul className="list-disc text-sm">
               {c?.children?.map((cc, k) => (
                 <li
                   key={k}
                   className="border-b py-1.5 px-2.5 flex items-center justify-between"
                 >
-                  <span>{cc?.title}</span>
-                  <div>
-                    <button className="p-1.5 rounded-full hover:bg-orange-50">
-                      <BiEdit className="text-orange-500 text-xl" />
-                    </button>
-                    <button onClick={()=> deleteCategory(c?.id)} className="p-1.5 rounded-full hover:bg-red-100">
-                      <MdDelete className="text-red-500 text-xl" />
-                    </button>
-                  </div>
+                  {editingSubId === cc?.id ? (
+                    <>
+                      {/* Child Category Name Update Input Field */}
+                      <div className="flex items-center gap-1 w-full">
+                        <input
+                          type="text"
+                          value={editedSubTitle}
+                          onChange={(e) => setEditedSubTitle(e.target.value)}
+                          className="px-1.5 py-1 border text-sm rounded w-full focus:outline-none"
+                        />
+                        <button
+                          onClick={() => updateSubCategory(cc?.parent_id)}
+                          className="p-1.5 rounded-full bg-green-50"
+                        >
+                          <BiCheck className="text-green-500 text-xl" />
+                        </button>
+                        <button
+                          onClick={() => {
+                            setEditingSubId(null);
+                            setEditedSubTitle("");
+                          }}
+                          className="p-1.5 rounded-full bg-red-50"
+                        >
+                          <CgClose className="text-red-500 text-xl" />
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      {/* General Child Category Name, Edit & Delete Button */}
+                      <span>{cc?.title}</span>
+                      <div>
+                        <button
+                          className="p-1.5 rounded-full hover:bg-orange-50"
+                          onClick={() => {
+                            setEditingSubId(cc?.id);
+                            setEditedSubTitle(cc?.title);
+                          }}
+                        >
+                          <BiEdit className="text-orange-500 text-xl" />
+                        </button>
+                        <button
+                          onClick={() => deleteCategory(c?.id)}
+                          className="p-1.5 rounded-full hover:bg-red-100"
+                        >
+                          <MdDelete className="text-red-500 text-xl" />
+                        </button>
+                      </div>
+                    </>
+                  )}
                 </li>
               ))}
             </ul>
+
             {parentId === c?.id ? (
               <div className="px-2 mt-2 flex gap-1 items-center">
                 <input
@@ -145,17 +265,29 @@ export default function AddCategory() {
                   onChange={(e) => setTitle(e.target.value)}
                 />
                 <div className="flex items-center gap-1">
+                  {/* cancel btn to add new category */}
                   <button
                     onClick={() => setParentId("")}
                     className="p-1.5 rounded-full bg-orange-50"
                   >
                     <CgClose className="text-orange-500 text-xl" />
                   </button>
-                  <button className="p-1.5 rounded-full bg-red-100">
-                    <BiCheck
-                      onClick={handleAddCategory}
-                      className="text-red-500 text-xl"
-                    />
+
+                  {/* add new category */}
+                  <button
+                    className={`p-1.5 rounded-full  ${
+                      loader
+                        ? "cursor-default bg-gray-100"
+                        : "cursor-pointer bg-red-50"
+                    }`}
+                    onClick={handleAddCategory}
+                    disabled={loader}
+                  >
+                    {loader ? (
+                      <ImSpinner className="text-lg text-gray-600 animate-spin" />
+                    ) : (
+                      <BiCheck className="text-red-500 text-xl" />
+                    )}
                   </button>
                 </div>
               </div>
--- src/Adminpage/AddProduct.jsx
+++ src/Adminpage/AddProduct.jsx
@@ -1,17 +1,77 @@
 import { Button, Spinner } from "@material-tailwind/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoCloseCircleSharp, IoImagesSharp } from "react-icons/io5";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useNavigate } from "react-router-dom";
 
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video", "code-block"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+
 const AddProduct = () => {
   const navigate = useNavigate();
   const [loader, setLoader] = useState(false);
+  const [categories, setCategories] = useState([]);
   const [value, setValue] = useState(null);
   const [title, setTitle] = useState(null);
   const [metaDesc, setMetaDesc] = useState(null);
   const [images, setImages] = useState(null);
+
+  const [formData, setFormData] = useState({
+    title: "",
+    category_id: "",
+    price: "",
+    description: "",
+    discount: "",
+    quantity: "",
+    is_featured: "",
+    is_best_selling: "",
+    on_flash_sale: "",
+    shipping_charge: "",
+    attributesSize: "",
+  });
+
+  // Fetch all categories
+  const fetchCategories = () => {
+    setLoader(true);
+    fetch("https://api.talukderhomes.com.au/api/categories")
+      .then((res) => res.json())
+      .then((data) => {
+        setLoader(false);
+        setCategories(data.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   // Function to handle file input change
   const handleImageChange = (e) => {
     const fileList = e.target.files;
@@ -24,39 +84,11 @@ const AddProduct = () => {
     setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   };
 
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, 3, 4, 5, 6] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      ["link", "image", "video", "code-block"],
-      ["clean"],
-    ],
-  };
-
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-  ];
-
-  // add a new service
+  // Add new product
   const addService = async (e) => {
-    setLoader(true);
     e.preventDefault();
+
+    /* setLoader(true);
     const formData = new FormData();
     images.forEach((image, index) => {
       formData.append(`image[${index}]`, image);
@@ -85,8 +117,9 @@ const AddProduct = () => {
     } catch (error) {
       console.error("Error:", error);
       setLoader(false);
-    }
+    } */
   };
+
   return (
     <form
       className="mt-5 md:mt-0 md:p-5 lg:p-10 flex flex-col gap-2.5"
@@ -96,8 +129,14 @@ const AddProduct = () => {
         <h5 className="text-xl md:text-3xl text-orange-600 font-semibold">
           Add Product
         </h5>
+
         <Button
-          disabled={images === null || value === null || title === null || metaDesc === null}
+          disabled={
+            images === null ||
+            value === null ||
+            title === null ||
+            metaDesc === null
+          }
           type="submit"
           className="bg-orange-600 flex gap-2 items-center"
         >
@@ -105,8 +144,15 @@ const AddProduct = () => {
           {loader && <Spinner className="h-4 w-4" />}
         </Button>
       </div>
+
+      {/* image field */}
       <div className="flex flex-col gap-2.5">
-        <label className="font-semibold">Select Image <span className="text-xs font-semibold text-red-500">(Maximum Image Size is 2MB)</span> </label>
+        <label className="font-semibold">
+          Select Image{" "}
+          <span className="text-xs font-semibold text-red-500">
+            (Maximum Image Size is 2MB)
+          </span>{" "}
+        </label>
         <input
           type="file"
           accept="image/*"
@@ -115,11 +161,15 @@ const AddProduct = () => {
           style={{ display: "none" }}
           id="image-upload-input"
         />
-        <label htmlFor="image-upload-input" className="flex items-center gap-2 px-4 py-2 border shadow rounded hover:shadow-orange-600 w-fit cursor-pointer">
+        <label
+          htmlFor="image-upload-input"
+          className="flex items-center gap-2 px-4 py-2 border shadow rounded hover:shadow-orange-600 w-fit cursor-pointer"
+        >
           <IoImagesSharp className="text-xl text-lightColor text-orange-600" />
           <p className="text-xs font-semibold">Choose Images</p>
         </label>
       </div>
+
       {/* Render preview of uploaded images */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4">
         {images?.map((image, index) => (
@@ -136,6 +186,8 @@ const AddProduct = () => {
           </div>
         ))}
       </div>
+
+      {/* name field */}
       <div className="flex flex-col gap-2.5">
         <label className="font-semibold">Enter Name</label>
         <input
@@ -143,10 +195,114 @@ const AddProduct = () => {
           name="title"
           className="px-4 py-2 outline-none border border-gray-400 rounded"
           placeholder="Enter Name"
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, title: e.target.value }))
+          }
           required
         />
       </div>
+
+      {/* category select dropdown */}
+      <div className="flex flex-col gap-2.5">
+        <label className="font-semibold">Category</label>
+        <select
+          className="px-4 py-2 outline-none border border-gray-400 rounded"
+          value={formData.category_id}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, category_id: e.target.value }))
+          }
+        >
+          <option value="" disabled>
+            --- Please select a category ---
+          </option>
+          {categories?.map((category) => (
+            <option key={category?.id} value={category?.id}>
+              {category?.title}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2.5 gap-x-5">
+        {/* price */}
+        <div className="flex flex-col gap-2.5">
+          <label className="font-semibold">Price</label>
+          <input
+            type="text"
+            name="price"
+            className="px-4 py-2 outline-none border border-gray-400 rounded"
+            placeholder="Enter Price"
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, price: e.target.value }))
+            }
+            required
+          />
+        </div>
+
+        {/* discount price */}
+        <div className="flex flex-col gap-2.5">
+          <label className="font-semibold">Discount Price</label>
+          <input
+            type="text"
+            name="discount"
+            className="px-4 py-2 outline-none border border-gray-400 rounded"
+            placeholder="Enter Discount Price"
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, discount: e.target.value }))
+            }
+            required
+          />
+        </div>
+
+        {/* shipping charge */}
+        <div className="flex flex-col gap-2.5">
+          <label className="font-semibold">Shipping Charge</label>
+          <input
+            type="text"
+            name="shipping"
+            className="px-4 py-2 outline-none border border-gray-400 rounded"
+            placeholder="Enter Shipping Charge"
+            onChange={(e) =>
+              setFormData((prev) => ({
+                ...prev,
+                shipping_charge: e.target.value,
+              }))
+            }
+            required
+          />
+        </div>
+
+        {/* quantity */}
+        <div className="flex flex-col gap-2.5">
+          <label className="font-semibold">Quantity</label>
+          <input
+            type="text"
+            name="quantity"
+            className="px-4 py-2 outline-none border border-gray-400 rounded"
+            placeholder="Enter Quantity"
+            onChange={(e) =>
+              setFormData((prev) => ({
+                ...prev,
+                quantity: e.target.value,
+              }))
+            }
+            required
+          />
+        </div>
+      </div>
+
+      {/* featured */}
+      <div className="flex flex-col gap-2.5">
+        <label>Featured</label>
+        <div className="flex items-center gap-2">
+          <input type="checkbox" name="featured" id="featuredYes" />{" "}
+          <label htmlFor="featuredYes">Yes</label>
+          <input type="checkbox" name="featured" id="featuredNo" />{" "}
+          <label htmlFor="featuredNo">No</label>
+        </div>
+      </div>
+
+      {/* meta description field */}
       <div className="flex flex-col gap-2.5">
         <label className="font-semibold">Enter Meta Description</label>
         <textarea
