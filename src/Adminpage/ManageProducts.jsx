df4845a56d8cd561e7dd7a09a6c6d628d7e69557
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2025-04-25 17:30:20 +0600

    feat: products table and delete feat added

--- src/Adminpage/ManageProducts.jsx
+++ src/Adminpage/ManageProducts.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import LoaderPage from "../Adminpage/LoaderPage";
+import ProductRow from "../components/admin/ProductRow";
 
 const ManageProducts = () => {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
-  console.log('====================================');
-  console.log(products);
-  console.log('====================================');
+
   //get products
   useEffect(() => {
     setLoading(true);
@@ -20,45 +18,42 @@ const ManageProducts = () => {
         }
       });
   }, []);
-  // delete service
-  const deleteService = async (id) => {
-    try {
-      const response = await fetch(
-        `https://api.talukderhomes.com.au/api/products/delete/${id}`,
-        {
-          method: "GET",
-        }
-      );
-      const data = await response.json();
-      if (data.status === true) {
-        const updatedProducts = products.filter((s) => s.id !== id);
-        setProducts(updatedProducts);
-        window.alert("Product deleted successfully!");
-      }
 
-      // Handle response data as needed
-    } catch (error) {
-      console.error("Error:", error);
-      setLoader(false);
-    }
-  };
   return (
     <section className="mt-5 md:mt-0 md:p-5 lg:p-10">
       {loading ? (
         <LoaderPage />
       ) : (
         <>
-          <h5 className="p-5 text-xl font-semibold text-blue">
-            Products: {products?.length}
+          <h5 className="px-5 py-2.5 text-xl font-semibold text-orange-500">
+            Total Products: {products?.length || 0}
           </h5>
-          <table>
-            <thead>
-              <th>
-                <td>Product</td>
-                <td>Quantity</td>
-              </th>
-            </thead>
-          </table>
+          {products && products.length > 0 ? (
+            <table className="w-full overflow-x-auto">
+              <thead className="bg-orange-50">
+                <tr>
+                  <th className="px-2.5 py-2">Product</th>
+                  <th className="px-2.5 py-2">Price</th>
+                  <th className="px-2.5 py-2">Discount (%)</th>
+                  <th className="px-2.5 py-2">Shipping</th>
+                  <th className="px-2.5 py-2">Quantity</th>
+                  <th className="px-2.5 py-2">Category</th>
+                  <th className="px-2.5 py-2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((product) => (
+                  <ProductRow
+                    key={product.id}
+                    product={product}
+                    setProducts={setProducts}
+                  />
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p className="px-5 text-xl font-semibold">No Products added yet!</p>
+          )}
         </>
       )}
     </section>
---
+++ src/components/admin/ProductRow.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { FiEdit } from "react-icons/fi";
+import { MdDeleteOutline } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+export default function ProductRow({ product, setProducts }) {
+  const {
+    id,
+    thumbnail,
+    title,
+    price,
+    discount,
+    shipping_charge,
+    quantity,
+    category,
+  } = product;
+  const [loading, setLoading] = useState(false);
+
+  const handleDelete = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch(
+        `https://api.talukderhomes.com.au/api/products/delete/${id}`,
+      );
+      const data = await res.json();
+      if (data.status) {
+        setProducts((prevProducts) =>
+          prevProducts.filter((product) => product.id !== id),
+        );
+      }
+    } catch (error) {
+      console.error(`product delete: ${error}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <tr className="border-b border-gray-200 last:border-b-0">
+      <td className="px-2.5 py-2">
+        <div className="flex items-center gap-2.5">
+          <Link>
+            <img
+              className="mx-auto size-11 rounded object-contain"
+              src={thumbnail}
+              alt={`image of ${title}`}
+              loading="lazy"
+            />
+          </Link>
+          <p>{title}</p>
+        </div>
+      </td>
+      <td className="px-2.5 py-2 text-center">{price}</td>
+      <td className="px-2.5 py-2 text-center">{discount}</td>
+      <td className="px-2.5 py-2 text-center">{shipping_charge}</td>
+      <td className="px-2.5 py-2 text-center">{quantity}</td>
+      <td className="px-2.5 py-2 text-center">{category?.title}</td>
+      <td className="px-2.5 py-2 text-center">
+        <div className="inline-flex items-center justify-center gap-2.5">
+          <button className="w-full" disabled={loading}>
+            <FiEdit
+              className={`transition-all duration-200 ease-in-out hover:text-green-500 ${loading ? "cursor-not-allowed" : "cursor-pointer"}`}
+            />
+          </button>
+
+          <button className="w-full" onClick={handleDelete} disabled={loading}>
+            <MdDeleteOutline
+              className={`text-xl transition-all duration-200 ease-in-out hover:text-orange-500 ${loading ? "cursor-not-allowed" : "cursor-pointer"}`}
+            />
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
+}
