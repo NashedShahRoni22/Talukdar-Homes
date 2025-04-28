e7e241f5bc30f28ed5478f5c1e967eac79d7a2a1
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2025-04-28 10:08:22 +0600

    bugs: adding blog giving 500 error response

--- src/Adminpage/AddBlog.jsx
+++ src/Adminpage/AddBlog.jsx
@@ -1,78 +1,84 @@
-import { Button, Spinner } from "@material-tailwind/react";
 import { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
 import { useNavigate } from "react-router-dom";
+import ReactQuill from "react-quill";
+import { Button, Spinner } from "@material-tailwind/react";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video", "code-block"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
 
 const AddBlog = () => {
   const navigate = useNavigate();
   const [loader, setLoader] = useState(false);
-  const [value, setValue] = useState("");
-  const [icon, setIcon] = useState("");
-  const [thumbnail, setThumbnail] = useState("");
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, 3, 4, 5, 6] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      ["link", "image", "video", "code-block"],
-      ["clean"],
-    ],
-  };
+  const [content, setContent] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
 
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-  ];
-
-  // add a new service
-  const addService = async (e) => {
-    setLoader(true);
+  // add a new blog submit
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setLoader(true);
+
     const title = e.target.title.value;
-    const price = e.target.price.value;
+    const slogan = e.target.slogan.value;
+
     const formData = new FormData();
-    formData.append("icon", icon);
     formData.append("title", title);
-    formData.append("thumbnail", thumbnail);
-    formData.append("price", price);
-    formData.append("content", value);
+    formData.append("slogan", slogan);
+    formData.append("image", thumbnail);
+    formData.append("content", content);
+
     try {
-      const response = await fetch("https://api.talukderhomes.com.au/api/blogs/store", {
-        method: "POST",
-        body: formData,
-        headers: {
-          // Add any necessary headers, such as authorization
-        },
-      });
+      const response = await fetch(
+        "https://api.talukderhomes.com.au/api/blogs/store",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
       const data = await response.json();
       if (data.status === true) {
         window.alert(data.msg);
         setLoader(false);
-        navigate("/admin/manageService");
+        navigate("/admin/manage-blog");
       }
       // Handle response data as needed
     } catch (error) {
       console.error("Error:", error);
       setLoader(false);
+    } finally {
+      setLoader(false);
     }
   };
+
   return (
-    <form className="mt-5 md:mt-0 md:p-5 lg:p-10" onSubmit={addService}>
+    <form className="mt-5 md:mt-0 md:p-5 lg:p-10" onSubmit={handleSubmit}>
       <div className="flex justify-between">
         <h5 className="text-xl md:text-3xl text-orange-600 font-semibold">
           Add Blog
@@ -88,7 +94,7 @@ const AddBlog = () => {
           <input
             type="file"
             className=""
-            onChange={(e) => setIcon(e.target.files[0])}
+            onChange={(e) => setThumbnail(e.target.files[0])}
           />
         </div>
         <div className="flex flex-col gap-2.5">
@@ -104,8 +110,8 @@ const AddBlog = () => {
       <div className="mt-5 flex flex-col gap-2.5">
         <label>Enter Slogan</label>
         <textarea
-          type="number"
-          name="price"
+          type="text"
+          name="slogan"
           className="px-4 py-2 outline-none border border-gray-400 rounded"
           placeholder="Enter Slogan"
           rows={5}
@@ -115,8 +121,8 @@ const AddBlog = () => {
         <label className="">Enter Content</label>
         <ReactQuill
           theme="snow"
-          value={value}
-          onChange={setValue}
+          value={content}
+          onChange={setContent}
           modules={modules}
           formats={formats}
           className="py-2 w-full h-[300px]"
